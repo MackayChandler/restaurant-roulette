ab37d2a4d7e98795b7bf89c430b3dac2c765907c
MackayChandler <55098733+MackayChandler@users.noreply.github.com>
2023-08-31 13:59:17 -0600

    feat: poc finished

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,45 @@ import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
+export interface Restaurant {
+  id: string;
+  name: string;
+  display_phone: string;
+  phone: string;
+  price: string;
+  rating: number;
+  review_count: number;
+  url: string;
+}
 
 export default function Home() {
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [selRestaurant, setSelRestaurant] = useState<Restaurant | null>(null);
   useEffect(() => {
-    getRestaurants();
+    let latitude = 0;
+    let longitude = 0;
+
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      await getRestaurants({ latitude, longitude, setRestaurants });
+    });
   }, []);
+
+  const pickRestaurant = () => {
+    const random = Math.floor(Math.random() * restaurants.length);
+    setSelRestaurant(restaurants[random]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Restaurant Roulette</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-5xl font-bold mb-5">Restaurant Roulette</h1>
+      <h2 className="text-2xl mb-5">{selRestaurant?.name}</h2>
+      <button
+        onClick={pickRestaurant}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Pick a restaurant
+      </button>
     </main>
   );
 }
-
-
-
-
--- services/services.ts
+++ services/services.ts
@@ -1,16 +1,28 @@
-import axios from "axios"
+import { Restaurant } from "@/app/page";
+import axios from "axios";
 
-export const getRestaurants = async () => {
-    axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search', {
-        headers: {
-            Authorization: `Bearer uF79f0BSjm9EWOg-uIInFeEwmxkpAHmGxXHJl7RpjP6EftSPBvGGPd-qfqx-MzFral4Pd1hMQWu9b9Pbfvkf7AeYZ0dA3RMudHLG6Au3Hg4eZ953VCi5QrdJCIqSYXYx`
-        },
-        params: {
-            location: 'New York City',
-        }
-    })
-    .then(response => {
-        console.log(response)
-    
-    })
+interface filterProps {
+  latitude?: number;
+  longitude?: number;
+  setRestaurants: (restaurants: Restaurant[]) => void;
+}
+
+export function getRestaurants({ latitude, longitude, setRestaurants }: filterProps) {
+  axios.get(
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search",
+    {
+      headers: {
+        Authorization: `Bearer uF79f0BSjm9EWOg-uIInFeEwmxkpAHmGxXHJl7RpjP6EftSPBvGGPd-qfqx-MzFral4Pd1hMQWu9b9Pbfvkf7AeYZ0dA3RMudHLG6Au3Hg4eZ953VCi5QrdJCIqSYXYx`,
+      },
+      params: {
+        latitude: latitude,
+        longitude: longitude,
+      },
+    }
+  ).then((res) => {
+    console.log(res.data.businesses)
+    setRestaurants(res.data.businesses)
+  })
+
+  
 }
